15b2130108c194b1efc5f2b13cfb306be5971d6c
taewookimm <kimtaewoo8487@gmail.com>
2024-08-22 22:24:57 +0900

    컬러셋 세팅

--- lib/gen/colors.gen.dart
+++ lib/gen/colors.gen.dart
@@ -14,6 +14,20 @@ import 'package:flutter/material.dart';
 class ColorName {
   ColorName._();
 
+  /// Color: #F5F9E1
+  static const Color backgroundColor = Color(0xFFF5F9E1);
+
+  /// Color: #F9B341
+  static const Color pointOrange = Color(0xFFF9B341);
+
+  /// Color: #22251c
+  static const Color defaultBlack = Color(0xFF22251c);
+
+  /// Color: #787D6F
+  static const Color defaultGray = Color(0xFF787D6F);
+
+
+  /// 임시 컬러 색상들 TODO: 나중에 지울 임시 컬러셋
   /// Color: #000000
   static const Color black = Color(0xFF000000);
 
