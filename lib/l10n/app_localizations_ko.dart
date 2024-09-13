190b69dc317804afc3b82b89b2cb42fb13599423
hyo <kdhyo98@gmail.com>
2024-09-13 18:20:04 +0900

    lint fix 포기

--- lib/l10n/app_localizations_ko.dart
+++ lib/l10n/app_localizations_ko.dart
@@ -2,7 +2,7 @@ import 'app_localizations.dart';
 
 /// The translations for Korean (`ko`).
 class AppLocalizationsKo extends AppLocalizations {
-  AppLocalizationsKo([super.locale = 'ko']);
+  AppLocalizationsKo([String locale = 'ko']) : super(locale);
 
   @override
   String get appName => 'COUPLE BOOK';
