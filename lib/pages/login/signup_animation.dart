72eb720c0e260f48181850c1a5376b2c54b86232
hyo <kdhyo98@gmail.com>
2024-09-28 01:10:12 +0900

    import 정리

--- lib/pages/login/signup_animation.dart
+++ lib/pages/login/signup_animation.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
-import 'package:couple_book/router.dart';
+
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 class SignupAnimationPage extends StatefulWidget {
   // TODO 유저 정보 받아오기
