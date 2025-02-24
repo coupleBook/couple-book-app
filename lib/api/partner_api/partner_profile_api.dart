1567b9147104cc4d2a4eb7a0e3fd4bd780a6ca7a
hyo <kdhyo98@gmail.com>
2025-02-24 23:27:34 +0900

    파트너 프로필 이미지 조회 API 추가

---
+++ lib/api/partner_api/partner_profile_api.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+import 'package:logger/logger.dart';
+
+import '../session.dart';
+import '../user_api/profile_image_response_dto.dart';
+
+class PartnerProfileApi {
+  final _dio = Session().dio;
+  final logger = Logger();
+
+  /// ************************************************
+  /// 파트너 프로필 이미지 조회 API
+  /// ************************************************
+  Future<ProfileImageResponseDto> getUserProfileImage() async {
+    final Response<dynamic> response =
+        await _dio.get('/api/v1/partner/profile/image');
+
+    if (response.statusCode == 200) {
+      return ProfileImageResponseDto.fromJson(response.data);
+    } else {
+      throw Exception(
+          "Failed to get user profile image: ${response.statusCode}, ${response.data}");
+    }
+  }
+}
